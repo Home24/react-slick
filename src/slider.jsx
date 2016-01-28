f38336a9d804219091d8b499629ae61b57f114a5
Ievgen Kainov <ievgen.kainov@home24.de>
2016-01-28 16:52:12 +0100

    Remove responsive mixin and slick carousel dependencies

--- src/slider.jsx
+++ src/slider.jsx
@@ -3,41 +3,13 @@
 import React from 'react';
 import {InnerSlider} from './inner-slider';
 import assign from 'object-assign';
-import json2mq from 'json2mq';
-import ResponsiveMixin from 'react-responsive-mixin';
 import defaultProps from './default-props';
 
 var Slider = React.createClass({
-  mixins: [ResponsiveMixin],
   getInitialState: function () {
     return {
       breakpoint: null
     };
-  },
-  componentDidMount: function () {
-    if (this.props.responsive) {
-      var breakpoints = this.props.responsive.map(breakpt => breakpt.breakpoint);
-      breakpoints.sort((x, y) => x - y);
-
-      breakpoints.forEach((breakpoint, index) => {
-        var bQuery;
-        if (index === 0) {
-          bQuery = json2mq({minWidth: 0, maxWidth: breakpoint});
-        } else {
-          bQuery = json2mq({minWidth: breakpoints[index-1], maxWidth: breakpoint});
-        }
-        this.media(bQuery, () => {
-          this.setState({breakpoint: breakpoint});
-        });
-      });
-
-      // Register media query for full screen. Need to support resize from small to large
-      var query = json2mq({minWidth: breakpoints.slice(-1)[0]});
-
-      this.media(query, () => {
-        this.setState({breakpoint: null});
-      });
-    }
   },
   render: function () {
     var settings;
